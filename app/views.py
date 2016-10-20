80b546ef84392cc550129c743cd0cb0221a3be5c
FawazFarid <fawwazally@gmail.com>
2016-10-20 17:23:16 +0300

    refactored code

--- app/views.py
+++ app/views.py
@@ -16,10 +16,12 @@ class LyricsViews(object):
             'Authorization': 'Bearer I01LPneQJjxNd9qsMdOukM2Z64MzuUekMRnuxubPGWvpL90WncPVl0uOYMBpXDny'
         }
 
+        self.baseUrl = 'http://api.genius.com/'
+
     def search(self, search_criteria):
         # url encode the search string
         query = urllib.quote(str(search_criteria))
-        search_url = 'http://api.genius.com/search?q=' + query
+        search_url = self.baseUrl + 'search?q=' + query
         r = requests.get(search_url, headers=self.headers)
 
         data = r.json()
@@ -38,7 +40,7 @@ class LyricsViews(object):
         # if no copy exists, get it from the Api
         else:
 
-            song_url = 'http://api.genius.com/songs/' + str(song_id)
+            song_url = self.baseUrl + 'songs/' + str(song_id)
             r = requests.get(song_url, headers=self.headers)
 
             data = r.json()
