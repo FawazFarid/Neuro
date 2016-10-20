d4bd52035b04c5b0c158a17d36a7f9ca957ade36
FawazFarid <fawwazally@gmail.com>
2016-10-20 10:57:14 +0300

    Used SQLAlchemy for my database Model

--- app/models.py
+++ app/models.py
@@ -1,2 +1,37 @@
-class LyricsModels(object):
-	pass
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import create_engine
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    DateTime,
+    Float,
+    Text
+)
+
+Base = declarative_base()
+
+
+class DbAbsLayer(object):
+    def __init__(self):
+        self.engine = create_engine('sqlite:///neuro.db')
+
+    def createSession(self):
+        Session = sessionmaker()
+        self.session = Session.configure(bind=self.engine)
+
+
+class Song(Base):
+    __tablename__ = 'songs'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    lyrics = Column(Text)
+    artist_id = Column(Integer, ForeignKey('artists.id'))
+
+
+class Artist(Base):
+    __tablename__ = 'artists'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
